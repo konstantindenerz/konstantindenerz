798e772490748cf4a5642995b1d1f6f821cdf980
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-02-23 11:17:48 +0100

    feat(card): add qr code and icons

--- build.js
+++ build.js
@@ -9,20 +9,62 @@ const options = {
     padding: 1, margin: 1, borderStyle: 'round', borderColor: 'white'
 }
 
+const qrcode = `
+
+             █▀▀▀▀▀█ ▀▄▄▀▀ ▀ ▄ █▀▀▀▀▀█
+             █ ███ █   ▀ ██▀ █ █ ███ █
+             █ ▀▀▀ █  ▀▄▀▄▀█ █ █ ▀▀▀ █
+             ▀▀▀▀▀▀▀ █ █▄▀▄▀▄█ ▀▀▀▀▀▀▀
+             ▀▀▄▀█▄▀ ▄█▀▀█▄▄▄█▄▀▄▄▄▄▄▀
+             ▀█▄█ ▀▀█ ▄█▀█▀▄█▄ ▀▄▀█▄▄█
+             ▄██▄▄ ▀▄▄▀ ▄  ▄█▄ ▀ ▄  ▄▀
+             █▀███▄▀▄█ ▀▄█  ▀ ▄▄▄▀██▀█
+             ▀ ▀ ▀▀▀▀▄▀▀▀ ▀ ▀█▀▀▀█ ██ 
+             █▀▀▀▀▀█  ██▀ █▄▄█ ▀ █  ▄▀
+             █ ███ █ █ ▀▄▀ █▀███▀▀  ▄▄
+             █ ▀▀▀ █ ▄▀ ▀▀ ▄▄  ▄▄█▀███
+             ▀▀▀▀▀▀▀ ▀ ▀ ▀   ▀▀▀  ▀  ▀`
+
 const content = `                 ${chalk.white.bold('KONST∆NTIN DENERZ')}
                     
                           
-   ${chalk.grey(`#Angular #Material #UX ${chalk.green('⚡')}UI #nvim, #TS #SCSS`)} 
+   ${chalk.grey(`#Angular #Material #UX ${chalk.green('⚡')}UI #Neovim, #TS #SCSS`)} 
         
                  
 ${chalk.white('Job')}                  ${chalk.white('Consultant & Architect & Coach')}
 ${chalk.white('Twitter')}                 ${chalk.white(`https://twitter.com/${chalk.green('kdenerz')}`)}
 ${chalk.white('Web')}                   ${chalk.white(`https://${chalk.green('konstantin-denerz')}.com`)}
 ${chalk.white('Work')}     ${chalk.white(`https://thinktecture.com/${chalk.green('konstantin-denerz')}`)}
-${chalk.white('Github')}          ${chalk.white(`https://github.com/${chalk.green('konstantindenerz')}`)}
+${chalk.white('GitHub')}          ${chalk.white(`https://github.com/${chalk.green('konstantindenerz')}`)}
 ${chalk.white('LinkedIn')}   ${chalk.white(`https://linkedin.com/in/${chalk.green('konstantindenerz')}`)}
-${chalk.white('Card')}                           ${chalk.white(`npx ${chalk.green('konstantindenerz')}`)}`
+${chalk.white('Card')}                           ${chalk.white(`npx ${chalk.green('konstantindenerz')}`)}`;
+
+const icons = {
+    'Job': '  ',
+    'Twitter': '      ',
+    'Web': '󰟒  ',
+    'Work': '   ',
+    'GitHub': '     ',
+    'LinkedIn': '󰌻       ',
+    'Card': '   ',
+}
 
 const card = chalk.white(boxen(content, options))
 writeFileSync(join(__dirname, './bin/output'), card);
 console.log(card);
+
+const cardWithQRCode = chalk.white(boxen(`${content}${qrcode}`, options))
+writeFileSync(join(__dirname, './bin/output-q'), cardWithQRCode);
+console.log(cardWithQRCode);
+
+let contentWithIcons = content;
+Object.entries(icons).forEach(([key, value]) => {
+    contentWithIcons = contentWithIcons.replaceAll(chalk.white(key), chalk.red.bold(value));
+});
+const cardWithIcons = chalk.white(boxen(contentWithIcons, options))
+writeFileSync(join(__dirname, './bin/output-i'), cardWithIcons);
+console.log(cardWithIcons);
+
+const cardWithIconsAndQrCode = chalk.white(boxen(`${contentWithIcons}${qrcode}`, options))
+writeFileSync(join(__dirname, './bin/output-a'), cardWithIconsAndQrCode);
+console.log(cardWithIconsAndQrCode);
--- index.js
+++ index.js
@@ -4,6 +4,31 @@ import {readFileSync} from 'fs';
 import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
 
+const args = process.argv;
+
+if (args.includes('-h')) {
+    console.log(`\r\nUsage: npx konstantindenerz@latest [options]\r\n
+Options: 
+   -q              Show card with QR code
+   -i              Show card with icons (Requires font like nerd font)
+   -a              Show card with icons and QR code
+        `)
+
+    process.exit(0);
+}
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const output = readFileSync(join(__dirname, 'bin/output'), 'utf8');
+let contentPath = 'bin/output';
+if (args.includes('-q')) {
+    contentPath += '-q';
+}
+
+if (args.includes('-i')) {
+    contentPath += '-i';
+}
+
+if (args.includes('-a')) {
+    contentPath += '-a';
+}
+const output = readFileSync(join(__dirname, contentPath), 'utf8');
 console.log(output);
